059655a74441f3eb4886f9bbc1740b2459118712
invnt <logan@invnt.co.za>
2022-08-18 08:46:05 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(event.target.elements.name.value); // from elements property
+  console.log(event.target.surname.value); // or directly
+};
+
 root.render(
   <StrictMode>
     <App />
